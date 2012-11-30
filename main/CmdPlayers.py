d9b6fdead0936fb5acef11942d147fb3220ae362
czeslaws <czeslaws@interia.pl>
2012-11-30 19:01:06 +0100

    asdf

--- main/CmdPlayers.py
+++ main/CmdPlayers.py
@@ -4,7 +4,19 @@ Created on 30 Nov 2012
 @author: Czeslaw
 '''
 
+from CommandParser import *
+players = []
+
 def handle(args):
-    print "Command Players: %s::" % args
-    #return args.split('\n')[-1]
+    #print "Command Players: %s::" % args
+    args_list = args.splitlines()
+    for item in args_list:
+        print "Command Players item: %s::" % item
+        if (item == '['):
+            continue
+        if (item == ']'):
+            ret_data = args.join('\n')
+            print "Command Players returning: %s::" % ret_data
+            return ret_data
+        players.append(item)
     return args.split('\n')[1:]

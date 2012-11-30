5d032ee13ee7a13e9b3e1174bf7f8ced8ed692b6
mikel <mikel.pl@gmail.com>
2012-11-30 23:03:25 +0100

    commit

--- main/CmdCondottiere.py
+++ main/CmdCondottiere.py
@@ -10,5 +10,7 @@ import random
 def handle(args):
     resp = CommandParser.regions[random.randint(0,len(CommandParser.regions)-1)]
     print "Condottiere :%s:" % resp
+    while CommandParser.occupiedRegion[CommandParser.ix(resp)] == 1:
+        resp = CommandParser.regions[random.randint(0,len(CommandParser.regions)-1)]
     TCPCommunicator.sendMessage(resp)
     return args[1:]

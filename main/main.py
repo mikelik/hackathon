1e29498159785e3809e9c2a330f85e89b73d6f68
mikel <mikel.pl@gmail.com>
2012-11-30 12:19:24 +0100

    again new project

---
+++ main/main.py
@@ -0,0 +1,8 @@
+'''
+Created on 30-11-2012
+
+@author: mikel
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ test/test.py
@@ -0,0 +1,18 @@
+'''
+Created on 30-11-2012
+
+@author: mikel
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()

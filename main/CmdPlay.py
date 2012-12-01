b49ce5b5658ab77a1d683d4abc0137526a069467
czeslaws <czeslaws@interia.pl>
2012-12-01 05:12:27 +0100

    c22

--- main/CmdPlay.py
+++ main/CmdPlay.py
@@ -4,8 +4,10 @@ Created on 30 Nov 2012
 @author: Czeslaw
 '''
 
+import CommandParser
 import Logger
 
 def handle(args):
     Logger.log("Command %s::" % args[0])
+    Logger.log("Player %s played %s" % (CommandParser.currentPlayer, args[0].split()[1]))
     return args[1:]
--- main/CmdPlayer.py
+++ main/CmdPlayer.py
@@ -10,5 +10,5 @@ import Logger
 def handle(args):
     Logger.log("Command %s::" % args[0])
     CommandParser.currentPlayer=args[0].split()[1]
-    Logger.log(CommandParser.currentPlayer)
+    #Logger.log(CommandParser.currentPlayer)
     return args[1:]
